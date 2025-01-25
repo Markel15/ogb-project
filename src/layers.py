1cf6f41798c167490c9611263da529852d0f4e7a
Markel15 <Enemar.h@gmail.com>
2025-01-25 23:00:22 +0100

    Actualizar capa convolucional

--- src/layers.py
+++ src/layers.py
@@ -1,11 +1,16 @@
 import torch
 from torch_geometric.nn.conv import MessagePassing
 
-class GraphConvolution(MessagePassing):
-    def __init__(self, emb_dim):
-        super(GraphConvolution, self).__init__() # Por defecto la agregación se hace con add
+class GraphConvolution(MessagePassing): 
+    #Al heredar de MessagePassing hay que definir los metodos de forward, message y update como mínimo
+
+    def __init__(self, dim_repr_nodo,metodo_agregacion):
+        super(GraphConvolution, self).__init__(aggr=metodo_agregacion) # Por defecto la agregación se hace con add (se puede cambiar)
 
         # Definir los parámetros a aprender en el proceso de MessagePassing, torch.nn.Linear es una definición parecida a la siguiente https://github.com/tkipf/pygcn/blob/master/pygcn/layers.py
-        self.node_transformer = torch.nn.Linear(emb_dim, emb_dim)
-        self.root_embedding = torch.nn.Embedding(1, emb_dim)
-        self.edge_transformer = torch.nn.Linear(7, emb_dim)
+        # Solo vamos a usar como representación a aprender en el MessagePassing las de cada nodo y las de las aristas
+        self.node_transformer = torch.nn.Linear(dim_repr_nodo, dim_repr_nodo)
+        self.edge_transformer = torch.nn.Linear(7, dim_repr_nodo)
+    
+        
+    
